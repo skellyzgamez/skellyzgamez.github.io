10ad41a5de3560e3b64a53b80b96b1f7715c9896
twrk4skelly <pradawashed@gmail.com>
2024-05-15 13:19:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// script.js
+// Example: Embedding a game (replace iframe src with your game URL)
+document.getElementById('game-container').innerHTML = '<iframe src="https://example.com/game" frameborder="0"></iframe>';
